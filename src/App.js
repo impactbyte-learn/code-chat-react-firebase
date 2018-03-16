76915b2304cc7cf89adcb133b1f471a1061ded75
M Haidar Hanif <git@mhaidarhanif.com>
2018-03-16 16:04:32 +0700

    Refactor to use object username & text

--- src/App.js
+++ src/App.js
@@ -16,10 +16,14 @@ class App extends Component {
     const messagesRef = database
       .ref("messages")
       .orderByKey()
-      .limitToLast(100);
+      .limitToLast(10);
 
     messagesRef.on("child_added", snapshot => {
-      const message = { text: snapshot.val(), id: snapshot.key };
+      const message = {
+        username: snapshot.val().username,
+        text: snapshot.val().text,
+        id: snapshot.key
+      };
 
       this.setState(prevState => ({
         messages: [message, ...prevState.messages]
@@ -30,22 +34,42 @@ class App extends Component {
   onAddMessage(event) {
     event.preventDefault();
 
-    database.ref("messages").push(this.input.value);
+    database.ref("messages").push({
+      username: this.username.value,
+      text: this.text.value
+    });
 
-    this.input.value = "";
+    this.text.value = "";
   }
 
   render() {
     return (
-      <form onSubmit={this.onAddMessage}>
-        <input type="text" ref={node => (this.input = node)} />
-        <input type="submit" />
+      <div>
+        <form onSubmit={this.onAddMessage}>
+          <label htmlFor="username">username:</label>
+          <input
+            type="text"
+            placeholder="username"
+            ref={node => (this.username = node)}
+          />
+          <br />
+          <label htmlFor="text">text:</label>
+          <input type="text" ref={node => (this.text = node)} />
+          <br />
+          <input type="submit" />
+        </form>
+
         <ul>
           {this.state.messages.map(message => (
-            <li key={message.id}>{message.text}</li>
+            <li key={message.id}>
+              {`"`}
+              {message.text}
+              {`"`}
+              <small style={{ color: "gray" }}> by {message.username}</small>
+            </li>
           ))}
         </ul>
-      </form>
+      </div>
     );
   }
 }
